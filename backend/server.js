ee509802fa36507ebd3922f66a80d852d43f9082
ZAINZESHAN <153626862+ZAINZESHAN@users.noreply.github.com>
2025-08-07 20:46:43 +0500

    ok

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,32 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import serverless from 'serverless-http'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
+import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
 
-import connectDB from '../config/mongodb.js'
-import connectCloudinary from '../config/cloudinary.js'
-import userRouter from '../routes/userRoute.js'
-import productRouter from '../routes/productRoute.js'
-import cartRouter from '../routes/cartRoute.js'
-import orderRouter from '../routes/orderRoute.js'
-
+// App config
 const app = express()
+const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
+
+// middlewares
 
 app.use(express.json())
 app.use(cors())
 
+// api endpoints
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
 app.use('/api/cart', cartRouter)
 app.use('/api/order', orderRouter)
 
 app.get('/', (req, res) => {
-  res.send('API Working')
+    res.send("API Working")
 })
 
-export const handler = serverless(app)
+app.listen(port, () => console.log('Server started on PORT : ' + port))
